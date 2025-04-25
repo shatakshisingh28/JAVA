950c7fc40f8ec7f569da1e20aae81cec8f0ca83e
shatakshisingh28 <138718195+shatakshisingh28@users.noreply.github.com>
2025-04-25 23:48:38 +0530

    Update clearbit.java

--- Bit Manipuation/clearbit.java
+++ Bit Manipuation/clearbit.java
@@ -6,6 +6,8 @@ public class clearbit {
         int n=sc.nextInt();
         System.out.println("Enter the bit you want to clear");
         int i=sc.nextInt();
-        
+        int s=n&~(1<<i);
+        System.out.println(s);
+        sc.close();
     }
 }
